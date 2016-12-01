cf6c7203efdcf5c5f22642bc76cbe7661f9bd393
PurpleBabar <lalung.alexandre@gmail.com>
2016-12-01 08:11:35 +0100

    Adding basic kahlan functions

---
+++ Essai.php
@@ -0,0 +1,4 @@
+<?php
+beforeEach(function() {
+    ;
+});
---
+++ spec/atom-kahlan-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomKahlanView from '../lib/atom-kahlan-view';
+
+describe('AtomKahlanView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
